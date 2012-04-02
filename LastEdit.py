3a8dd38841cbf05afe19c5d120984283e94c08b0
SamPeng87 <405614104@qq.com>
2012-04-02 14:12:10 +0800

    first commit

---
+++ LastEdit.py
@@ -0,0 +1,94 @@
+import sublime, sublime_plugin
+
+class LastEditCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        viewsCount = len(RecordIntputRegion.lastView)-1;
+
+
+        lastView = RecordIntputRegion.lastView[viewsCount];
+        lastViewPosKey = str(lastView)+":"+str(viewsCount);
+        if len(RecordIntputRegion.lastLine[lastViewPosKey]) >0:
+            lastViewLine = RecordIntputRegion.lastLine[lastViewPosKey].pop()
+        else:
+            RecordIntputRegion.lastView.pop();
+            viewsCount = len(RecordIntputRegion.lastView)-1;
+            lastView = RecordIntputRegion.lastView[viewsCount];
+            lastViewPosKey = str(lastView)+":"+str(viewsCount);
+            lastViewLine = RecordIntputRegion.lastLine[lastViewPosKey].pop()
+
+        RecordIntputRegion.lastLine[lastViewPosKey].insert(0,lastViewLine)
+        lastViewCol = RecordIntputRegion.editLine[lastView][lastViewLine]
+
+        lastViewObj=None
+        for window in sublime.windows():
+            for view in window.views():
+                if lastView == view.id():
+                    lastViewObj = view
+                    lastWindow  = window
+                    pass
+        lastGroup,_ = lastWindow.get_view_index(lastViewObj)
+        lastWindow.focus_group(lastGroup) 
+
+        lastWindow.focus_view(lastViewObj);
+
+        pt = lastViewObj.text_point(lastViewLine,lastViewCol);
+
+        lastViewObj.sel().clear();
+
+        lastViewObj.sel().add(sublime.Region(pt));
+
+        lastViewObj.show(pt);
+
+
+        # pt=self.view.text_point(10,10);
+        # self.view.sel().clear();
+        # self.view.sel().add(sublime.Region(pt));
+
+        # self.view.show(pt);
+
+        # //sublime.error_message(str(lastEdit['file']));
+
+
+
+class RecordIntputRegion(sublime_plugin.EventListener):
+    editLine =   {}
+    lastLine =   {}
+    lastView = []
+
+    def on_modified(self, view):
+        sel = view.sel()[0]
+
+        curr_view = view.id()
+        curr_window = view.window().id()
+        last_line,last_col = view.rowcol(sel.begin())
+        lastViewPos = len(RecordIntputRegion.lastView)-1
+
+        if lastViewPos == -1:
+            RecordIntputRegion.lastView.append(curr_view)
+            lastViewPos += 1
+            pass
+        
+        if not RecordIntputRegion.lastView[lastViewPos] is curr_view:
+            RecordIntputRegion.lastView.append(curr_view)
+            lastViewPos += 1
+
+        viewPosKey = str(curr_view)+":"+str(lastViewPos)
+        if not RecordIntputRegion.lastLine.has_key(viewPosKey):
+            RecordIntputRegion.lastLine[viewPosKey]=[]
+            pass
+
+        lastLinePos = len(RecordIntputRegion.lastLine[viewPosKey]) -1;
+        
+
+        if lastLinePos == -1:
+            RecordIntputRegion.lastLine[viewPosKey].append(last_line);
+            lastLinePos +=1;
+
+        if not RecordIntputRegion.lastLine[viewPosKey][lastLinePos] is last_line:
+            RecordIntputRegion.lastLine[viewPosKey].append(last_line)
+            pass
+
+        if not RecordIntputRegion.editLine.has_key(curr_view):
+            RecordIntputRegion.editLine[curr_view]={}
+            pass
+        RecordIntputRegion.editLine[curr_view][last_line] = last_col
